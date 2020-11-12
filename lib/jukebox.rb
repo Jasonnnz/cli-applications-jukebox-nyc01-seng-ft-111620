431b96a13aead2f0ecf232071eccdabdd6c4e30a
Jason  Zhou <jzhou35@buffalo.edu>
2020-11-12 19:26:07 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -15,8 +15,9 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_input  = gets.strip
-  if songs.member?(user_input) || user_input.between?(1..songs.length)
-    puts "Playing #{}"
+  song.each_with_index do |song, index|
+    if songs.member?(user_input) || user_input.between?(1..songs.length)
+      puts "Playing #{}"
 end
     
 
